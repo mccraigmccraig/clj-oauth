37c7d1e9ad8e918483c0e1f73d626375194c9e82
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-01-19 17:41:54 +0000

    private snapshot groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-oauth "1.3.1-SNAPSHOT"
+(defproject org.clojars.mccraigmccraig/clj-oauth "1.3.1-SNAPSHOT"
   :description "OAuth support for Clojure"
   :repositories {"snapshots" {:url "s3p://lein-snapshots/snapshots"}}
   :dependencies [[org.clojure/clojure "1.3.0"]
